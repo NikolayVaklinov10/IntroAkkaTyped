3623d5d11a2f6bbd4d5e054e25dd1940ba67336d
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-05-26 16:15:07 +0100

    the initial part of the ChatRoom app with case classes for the supported messages and traits that extends them

---
+++ src/main/scala/IntroductionToActors/ChatRoom.scala
@@ -0,0 +1,18 @@
+package IntroductionToActors
+
+import akka.actor.typed.ActorRef
+
+object ChatRoom {
+  sealed trait RoomCommand
+  final case class GetSession(screenName: String, replyTo: ActorRef[SessionEvent]) extends RoomCommand
+
+  sealed trait SessionEvent
+  final case class SessionGranted(handle: ActorRef[PostMessage]) extends SessionEvent
+  final case class SessionDenied(reason: String) extends SessionEvent
+  final case class MessagePosted(screenName: String, message: String) extends SessionEvent
+
+  sealed trait SessionCommand
+  final case class PostMessage(message: String) extends SessionCommand
+  private final case class NotifyClient(message: MessagePosted) extends SessionCommand
+
+}
