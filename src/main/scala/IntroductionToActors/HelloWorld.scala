e06e45965786b3358aa70a91d4dc01d9f4dfca94
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-05-26 14:54:54 +0100

    the bot hello world actor

--- src/main/scala/IntroductionToActors/HelloWorld.scala
+++ src/main/scala/IntroductionToActors/HelloWorld.scala
@@ -14,5 +14,30 @@ object HelloWorld {
     message.replyTo ! Greeted(message.whom, context.self)
     Behaviors.same
   }
-
 }
+
+// The Hello world bot actor
+object HelloWorldBot {
+
+  def apply(max: Int): Behavior[HelloWorld.Greeted] = {
+    bot(0, max)
+  }
+
+  private def bot(greetingCounter: Int, max: Int): Behavior[HelloWorld.Greeted] =
+    Behaviors.receive { (context, message) =>
+      val n = greetingCounter + 1
+      //#fiddle_code
+      context.log.info2("Greeting {} for {}", n, message.whom)
+      //#fiddle_code
+      //#hello-world-bot
+      println(s"Greeting $n for ${message.whom}")
+      //#hello-world-bot
+      if (n == max) {
+        Behaviors.stopped
+      } else {
+        message.from ! HelloWorld.Greet(message.whom, context.self)
+        bot(n, max)
+      }
+    }
+}
+
